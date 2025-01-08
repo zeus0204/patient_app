2a838d2895a278c33681dc19fc289cdb26fcf78f
Nico Peter <nicopeter950101@gmail.com>
2025-01-08 14:24:45 -0800

    implement offline mode

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import './Pages/LandingPage/landing_page.dart';
 import 'package:firebase_core/firebase_core.dart';
@@ -5,6 +6,8 @@ import 'package:firebase_core/firebase_core.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp();
+  FirebaseFirestore.instance.settings =  
+      const Settings(persistenceEnabled: true);  
   runApp(const MyApp());
 }
 
